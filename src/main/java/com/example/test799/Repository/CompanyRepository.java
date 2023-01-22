820bb641e273bf021d16eb71c0a64a2470e9a429
waelalq <wael.abuomr@gmail.com>
2023-01-22 20:50:44 +0200

    Repository

--- src/main/java/com/example/test799/Repository/CompanyRepository.java
+++ src/main/java/com/example/test799/Repository/CompanyRepository.java
@@ -1,9 +1,9 @@
-package com.example.test799.Reposoory;
+package com.example.test799.Repository;
 
 import com.example.test799.Entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyReposotry extends JpaRepository<Company,Long> {
+public interface CompanyRepository extends JpaRepository<Company,Long> {
 }
