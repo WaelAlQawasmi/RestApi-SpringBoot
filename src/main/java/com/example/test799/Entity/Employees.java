e73df7f128e30bd89e62704f96c3cc194d70a349
waelalq <wael.abuomr@gmail.com>
2023-01-23 23:07:54 +0200

    Employees entity

---
+++ src/main/java/com/example/test799/Entity/Employees.java
@@ -0,0 +1,29 @@
+package com.example.test799.Entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+
+public class Employees {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @NonNull
+    String name;
+    @NonNull
+    int salary;
+    @NonNull
+    int startDate;
+
+
+
+
+}
