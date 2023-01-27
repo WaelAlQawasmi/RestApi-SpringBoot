c473e8c140388b4358ce25b0d6e4b2756fbd3e03
waelalq <wael.abuomr@gmail.com>
2023-01-27 18:39:13 +0200

    Employees servis

---
+++ src/main/java/com/example/restApi/Servises/EmployeesService.java
@@ -0,0 +1,9 @@
+package com.example.restApi.Servises;
+
+import com.example.restApi.Repository.EmployeesRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeesService {
+    EmployeesRepository employeesRepository;
+}
